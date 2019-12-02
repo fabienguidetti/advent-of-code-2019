adfbd33e206e2cc3a227ee7a2b0d8782301ee9bb
Fabien Guidetti <fabienguidetti@gmail.com>
2019-12-02 17:35:46 +0100

    Solved day 02 puzzle 2

---
+++ src/main/java/fabienguidetti/adventofcode2019/day02/Puzzle2.java
@@ -0,0 +1,28 @@
+package fabienguidetti.adventofcode2019.day02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Puzzle2 {
+	public static void main(String[] args) throws IOException {
+		String inputString = Files.readAllLines(Paths.get("day-02-input.txt")).get(0);
+		for (int noun = 0; noun <= 99; noun++) {
+			for (int verb = 0; verb <= 99; verb++) {
+				List<Integer> input = Utils.splitIntegers(inputString);
+				applyNounAndVerb(input, noun, verb);
+				List<Integer> output = Computer.execute(input);
+				if (output.get(0) == 19690720) {
+					System.out.println("Found noun : " + noun);
+					System.out.println("Found verb : " + verb);
+				}
+			}
+		}
+	}
+
+	private static void applyNounAndVerb(List<Integer> input, int noun, int verb) {
+		input.set(1, noun);
+		input.set(2, verb);
+	}
+}
